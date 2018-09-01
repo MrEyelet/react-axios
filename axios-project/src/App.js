2dfe0ad7b9a20c11a6e7570b1f2b65f2c235a40a
unknown <marcin.adam.mroczko@gmail.com>
2018-09-01 23:55:33 +0200

    adding url API

--- axios-project/src/App.js
+++ axios-project/src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 import UserForm from './UserForm';
 
 class App extends Component {
+  getUser = (e) => {
+    e.preventDefault();
+    const user = e.target.elements.username.value;
+    axios.get(`https://api.github.com/users/${user}`)
+    .then((res) => {
+      console.log(res);
+    })
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">HTTP Calls in React</h1>
         </header>
-        <UserForm />
+        <UserForm getUser={this.getUser} />
       </div>
     );
   }

b33c04226ee583c707007829c9bb315798017c36
unknown <marcin.adam.mroczko@gmail.com>
2018-09-02 00:35:37 +0200

    update axios app

--- axios-project/src/App.js
+++ axios-project/src/App.js
@@ -4,14 +4,27 @@ import './App.css';
 import UserForm from './UserForm';
 
 class App extends Component {
+  state ={
+    repos: null
+  }
   getUser = (e) => {
     e.preventDefault();
     const user = e.target.elements.username.value;
-    axios.get(`https://api.github.com/users/${user}`)
-    .then((res) => {
-      console.log(res);
-    })
+    if (user){
+
+      axios.get(`https://api.github.com/users/${user}`)
+      .then((res) => {
+      const repos = res.data.public_repos;
+      this.setState({repos: repos});
+      })
+
+    } else {
+
+      return
+
+    }
   }
+  
   render() {
     return (
       <div className="App">
@@ -19,6 +32,8 @@ class App extends Component {
           <h1 className="App-title">HTTP Calls in React</h1>
         </header>
         <UserForm getUser={this.getUser} />
+        {this.state.repos ? <h2>Number of repos: <span>{this.state.repos}</span></h2> :
+        <h2>Please enter a user name</h2>}
       </div>
     );
   }
--- axios-project/src/UserForm.js
+++ axios-project/src/UserForm.js
@@ -3,6 +3,7 @@ import React from 'react';
 const UserForm = (props) => {
 	return(
 		<div className="User-form">
+			<h1>Do you know how many github repositories do you have?</h1>
 			<form onSubmit={props.getUser}>
 				<input type="text" name="username"/>
 				<button>Submit</button>

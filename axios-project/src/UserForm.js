eff9d6accafc0164b881414a2b41e31fab52def1
unknown <marcin.adam.mroczko@gmail.com>
2018-09-02 13:33:18 +0200

    updates

--- axios-project/src/UserForm.js
+++ axios-project/src/UserForm.js
@@ -3,9 +3,9 @@ import React from 'react';
 const UserForm = (props) => {
 	return(
 		<div className="User-form">
-			<h1>Do you know how many github repositories do you have?</h1>
+			<h1 className="App-title">How many github repositories do you have?</h1>
 			<form onSubmit={props.getUser}>
-				<label>Your username</label>
+				<label>your username</label>
 				<input type="text" name="username"/>
 				<button>Submit</button>
 			</form>
